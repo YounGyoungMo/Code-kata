0143749a0c4c8d318a1019bdadda3ce826268425
YounGyoungMo <gyoungmo2011@naver.com>
2025-03-28 21:07:28 +0900

    03/29 code kata

---
+++ src/algorithm/Q23.java
@@ -0,0 +1,26 @@
+package algorithm;
+
+public class Q23 {
+    class Solution {
+        public int solution(int num) {
+            int count = 0;
+            while(num != 1) {
+                if (count >= 500) {
+                    return -1;
+                }
+
+                if (num % 2 == 0) {
+                    num /= 2;
+                }
+                else if(num % 2 == 1){
+                    num = num * 3 + 1;
+                }
+                count++;
+
+            }
+
+            return count;
+
+        }
+    }
+}
