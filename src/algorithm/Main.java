b63c523b9d394bc0d393a70fa356a25de62274e7
YounGyoungMo <gyoungmo2011@naver.com>
2025-04-01 23:36:55 +0900

    4/1 code kata

--- src/algorithm/Main.java
+++
@@ -1,8 +0,0 @@
-package algorithm;
-public class Main {
-
-    public static void main(String[] args) {
-        Q9 q1 = new Q9();
-        System.out.println(q1.solution(10));
-    }
-}
--- src/sql/Q7.sql
+++
@@ -1,5 +0,0 @@
-// 7. 아픈동물 찾기
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME
-FROM ANIMAL_INS
-WHERE INTAKE_CONDITION = 'Sick'
